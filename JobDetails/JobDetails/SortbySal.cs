5c8d119ce481cee391908ac7e1ba7cf574e0bb5d
Pavithra2304 <92969170+Pavithra2304@users.noreply.github.com>
2021-11-05 09:37:45 +0530

    Add files via upload

---
+++ JobDetails/JobDetails/SortbySal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobDetails
+{
+    class SortbySal : IComparer<Job>
+    {
+        public int Compare(Job x, Job y)
+        {
+            return x.Sal.CompareTo(y.Sal);
+        }
+
+
+    }
+}
